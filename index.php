f82e4a5bb12052d2a3d41eb6eeb2427ed1353c4f
billie115 <you@example.com>
2025-12-04 15:13:54 +0200

    commit vasilis

--- index.php
+++ index.php
@@ -14,18 +14,7 @@ echo "They do good work";
 
 <body>
     <br>
-    <h2>vasilis</h2>
-    <h1>stamatis</h1>
-    <h1>Car_agency App</h1>
-    <button>
-        order a nigger
-    </button>
-    <h1>efe eisai amea</h1>
-    <h1>You too </h1>
-    <h2>stamatis gay</h2>
-    <p>THAT WAS SIMPLE</p>
-    <button>HALLO </button>
-    <h1>vasilis is gay</h1>
+    <h1>Hello Nigger san</h1>
     <img src="Images/Scratch_ship_Game.png" alt="">
 
 </body>

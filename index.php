f39de2fb5782413d6a9e090b2410e1ba384a1b6b
ombasiefe <ombasiefe.2004@gmail.com>
2025-12-04 13:42:06 +0200

    commit 2 efe

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <?php
-    echo"I love niggers <br>";
-    echo"They do good work";
+echo "I love niggers <br>";
+echo "They do good work";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <br>
     <button>
         order a nigger
     </button>
     <h1>efe eisai amea</h1>
+    <h1>You too </h1>
 </body>
+
 </html>

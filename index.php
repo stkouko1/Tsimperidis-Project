f11acae7cb118de12599af8d2efee3da191d6d7c
billie115 <you@example.com>
2025-12-05 12:01:14 +0200

    tmep

--- index.php
+++ index.php
@@ -1,63 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
         rel="stylesheet">
-    <title>Κεντρική Σελίδα</title>
 </head>
-
 <body>
-    <div id="heading-div">
-        <h1 style="font-size:x-large">Αντιπροσωπία Αυτοκινήτων</h1>
-    </div><br>
-    <div style="display: flex; flex-wrap:wrap; align-content:center; justify-content:center; gap:10px">
-
-        <div class="menu-items" onclick="window.location.href='sub_sites/etairies.php'">
-            <i class="material-icons" style="font-size: 35px;">apartment</i>
-            <h2 style="margin-top:-3px ;">Εταιρίες </h2>
-            <p>Εταιρίες που συνεργαζόμαστε.</p>
-        </div>
-        <div class="menu-items" onclick="window.location.href='sub_sites/autokinhta.php'">
-            <i class="material-icons" style="font-size: 35px;">directions_car</i>
-            <h2 style="margin-top:-3px ;">Αυτοκίνητα</h2>
-            <p>Αυτοκίνητα της αντιπροσωπίας.</p>
-        </div>
-        <div class="menu-items"  onclick="window.location.href='sub_sites/pelates.php'">
-            <i class="material-icons" style="font-size: 35px;">group</i>
-            <h2 style="margin-top:-3px ;">Πελάτες</h2>
-            <p>Οι πελάτες τις αντιπροσωπίας μας.</p>
-        </div>
-        <div class="menu-items"  onclick="window.location.href='sub_sites/pwlhseis.php'">
-            <i class="material-icons" style="font-size: 35px;">account_balance</i>
-            <h2 style="margin-top:-3px ;">Πωλήσεις</h2>
-            <p>Πωλήσεις της επιχείρησης μας.</p>
-        </div>
-        <div class="menu-items"  onclick="window.location.href='sub_sites/sunthrish.php'">
-            <i class="material-icons" style="font-size: 35px;">car_repair</i>
-            <h2 style="margin-top:-3px ;">Συντήριση</h2>
-            <p>Πωλήσεις της επιχείρησης μας.</p>
-        </div>
-        <div class="menu-items"  onclick="window.location.href='sub_sites/mhxanikoi.php'">
-            <i class="material-icons" style="font-size: 35px;">engineering</i>
-            <h2 style="margin-top:-3px ;">Μηχανικοί</h2>
-            <p>Πωλήσεις της επιχείρησης μας.</p>
-        </div>
-        <div class="menu-items"  onclick="window.location.href='sub_sites/pwlhtes.php'">
-            <i class="material-icons" style="font-size: 35px;">badge</i>
-            <h2 style="margin-top:-3px ;">Πωλητές</h2>
-            <p>Υπάλληλοι πωλήσεων.</p>
-        </div>
-
-
-    </div>
+    <div class="menu-items" onclick="window.location.href='sub_sites/dashboard.php'">
+                <i class="material-icons" style="font-size: 35px;">apartment</i>
+                <h2 style="margin-top:-3px ;">Εταιρίες</h2>
+                <p>Εταιρίες που συνεργαζόμαστε.</p>
+            </div>
 </body>
-
-<footer>
-        Developed by ©KavalaCore 2025
-</footer>
-
 </html>
---
+++ sub_sites/dashboard.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet">
+    <title>Κεντρική Σελίδα</title>
+</head>
+
+<header>
+    <div id="heading-div">
+        <h1 style="font-size:x-large">Αντιπροσωπία Αυτοκινήτων</h1>
+    </div><br>
+</header>
+<main>
+    <body>
+         <div style="display: flex; flex-wrap:wrap; align-content:center; justify-content:center; gap:10px">
+            <div class="menu-items" onclick="window.location.href='etairies.php'">
+                <i class="material-icons" style="font-size: 35px;">apartment</i>
+                <h2 style="margin-top:-3px ;">Εταιρίες </h2>
+                <p>Εταιρίες που συνεργαζόμαστε.</p>
+            </div>
+            <div class="menu-items" onclick="window.location.href='autokinhta.php'">
+                <i class="material-icons" style="font-size: 35px;">directions_car</i>
+                <h2 style="margin-top:-3px ;">Αυτοκίνητα</h2>
+                <p>Αυτοκίνητα της αντιπροσωπίας.</p>
+            </div>
+            <div class="menu-items"  onclick="window.location.href='pelates.php'">
+                <i class="material-icons" style="font-size: 35px;">group</i>
+                <h2 style="margin-top:-3px ;">Πελάτες</h2>
+                <p>Οι πελάτες τις αντιπροσωπίας μας.</p>
+            </div>
+            <div class="menu-items"  onclick="window.location.href='pwlhseis.php'">
+                <i class="material-icons" style="font-size: 35px;">account_balance</i>
+                <h2 style="margin-top:-3px ;">Πωλήσεις</h2>
+                <p>Πωλήσεις της επιχείρησης μας.</p>
+            </div>
+            <div class="menu-items"  onclick="window.location.href='sunthrish.php'">
+                <i class="material-icons" style="font-size: 35px;">car_repair</i>
+                <h2 style="margin-top:-3px ;">Συντήριση</h2>
+                <p>Πωλήσεις της επιχείρησης μας.</p>
+            </div>
+            <div class="menu-items"  onclick="window.location.href='mhxanikoi.php'">
+                <i class="material-icons" style="font-size: 35px;">engineering</i>
+                <h2 style="margin-top:-3px ;">Μηχανικοί</h2>
+                <p>Πωλήσεις της επιχείρησης μας.</p>
+            </div>
+            <div class="menu-items"  onclick="window.location.href='pwlhtes.php'">
+                <i class="material-icons" style="font-size: 35px;">badge</i>
+                <h2 style="margin-top:-3px ;">Πωλητές</h2>
+                <p>Υπάλληλοι πωλήσεων.</p>
+            </div>
+        </div>
+    </body>
+</main>
+<footer>
+    Developed by ©KavalaCore 2025
+</footer>
+
+</html>
